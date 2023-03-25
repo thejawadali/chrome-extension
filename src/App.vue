277ff5ff1530b37e9784badbc42a3e3899111207
Jawad Ali <jawad.ali@repairdesk.co>
2023-03-25 16:20:00 +0500

    init project with tailwindcss setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-red-500">
+    ali
+  </div>
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  important: true,
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
